fc1debeee653f5623f30f502d42473b7b2d8366d
Ukubaevalan <ukubaevalan@gmail.com>
2026-06-12 12:17:40 +0900

    Your descriptive commit message

--- Hitman-Card-Game/hitman/EmptyDeckException.java
+++
@@ -1,7 +0,0 @@
-package hitman;
-
-public class EmptyDeckException extends Exception{
-	public EmptyDeckException(String message) {
-		super(message);
-	}
-}
--- Hitman-Card-Game/hitman/InvalidPlayException.java
+++
@@ -1,7 +0,0 @@
-package hitman;
-
-public class InvalidPlayException extends Exception{
-	public InvalidPlayException(String message) {
-		super(message);
-	}
-}
